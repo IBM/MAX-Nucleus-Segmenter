fe038acb58f385051ce8e1f625ea9373c0877a67
Brendan Dwyer <brendan.dwyer@ibm.com>
2019-03-29 22:53:20 -0700

    Update config.py
    Co-Authored-By: feihugis <fei.hu1@ibm.com>

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 
 # API Metadata
 API_TITLE = 'MAX Nucleus Segmenter'
-API_DESC = 'Identify nuclei in a microscopy image, additionally assigning each pixel of the image to a particular nucleus'
+API_DESC = 'Identify nuclei in a microscopy image and assign each pixel of the image to a particular nucleus'
 API_VERSION = '0.1'
 
 # default model
