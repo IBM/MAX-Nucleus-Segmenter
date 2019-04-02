12d13f73cba2a54b9150c4b8fb0c29eeac1f9c49
Nick Pentreath <nickp@za.ibm.com>
2019-04-02 08:02:29 +0100

    Bump config version to 1.0.0 for release

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 # API Metadata
 API_TITLE = 'MAX Nucleus Segmenter'
 API_DESC = 'Identify nuclei in a microscopy image and assign each pixel of the image to a particular nucleus'
-API_VERSION = '0.1'
+API_VERSION = '1.0.0'
 
 # default model
 MODEL_NAME = 'mask_rcnn_nucleus_detection'
